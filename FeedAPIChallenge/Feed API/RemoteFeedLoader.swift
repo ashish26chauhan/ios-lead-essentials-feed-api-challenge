eebf4f3aa918ee63786df3bc50ec7881b8ea72d2
Ashish Chauhan <ashish26chauhan@gmail.com>
2021-09-16 00:47:22 +0530

    Delivers success with items array on 200 response with json items

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,14 +22,42 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case let .success((data, response)):
-				if let _ = try? JSONSerialization.jsonObject(with: data), response.statusCode == 200 {
-					completion(.success([]))
-				} else {
+				do {
+					let items = try FeedImageMapper.map(data: data, response: response)
+					completion(.success(items))
+				} catch {
 					completion(.failure(RemoteFeedLoader.Error.invalidData))
 				}
 			case .failure(_):
 				completion(.failure(RemoteFeedLoader.Error.connectivity))
 			}
 		}
+	}
+}
+
+private class FeedImageMapper {
+	private struct Root: Decodable {
+		let items: [Item]
+	}
+
+	private struct Item: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+
+		var item: FeedImage {
+			return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+		}
+	}
+
+	class func map(data: Data, response: HTTPURLResponse) throws -> [FeedImage] {
+		guard response.statusCode == 200 else {
+			throw RemoteFeedLoader.Error.invalidData
+		}
+
+		let root = try JSONDecoder().decode(Root.self, from: data)
+
+		return root.items.map { $0.item }
 	}
 }
