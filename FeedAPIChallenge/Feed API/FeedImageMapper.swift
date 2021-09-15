e1a489a105094e0c9da83978473baa257a183933
Ashish Chauhan <ashish26chauhan@gmail.com>
2021-09-16 01:13:26 +0530

    Move FeedImageMapper to separate file

---
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -0,0 +1,38 @@
+//
+//  FeedImageMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by Ashish Chauhan on 16/09/21.
+//  Copyright © 2021 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+class FeedImageMapper {
+	private static var OK_200 = 200
+
+	private struct Root: Decodable {
+		let items: [Item]
+	}
+
+	private struct Item: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+
+		var item: FeedImage {
+			return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+		}
+	}
+
+	class func map(data: Data, response: HTTPURLResponse) throws -> [FeedImage] {
+		guard response.statusCode == OK_200 else {
+			throw RemoteFeedLoader.Error.invalidData
+		}
+
+		let root = try JSONDecoder().decode(Root.self, from: data)
+
+		return root.items.map { $0.item }
+	}
+}
